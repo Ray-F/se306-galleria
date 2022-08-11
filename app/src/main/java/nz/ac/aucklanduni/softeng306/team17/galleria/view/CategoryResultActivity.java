935c38aa9a2cded4ef19df0a4525322937a7967f
Raymond <xzha897@aucklanduni.ac.nz>
2022-08-11 23:20:24 +1200

    Add some color values needed for development

---
+++ app/src/main/java/nz/ac/aucklanduni/softeng306/team17/galleria/view/CategoryResultActivity.java
@@ -0,0 +1,13 @@
+package nz.ac.aucklanduni.softeng306.team17.galleria.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CategoryResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
