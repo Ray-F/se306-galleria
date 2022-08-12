2b7e8773bac663e405ee8830bb9b979bd0923989
Raymond Feng <rf.raymondfeng@gmail.com>
2022-08-12 18:20:55 +1200

    Improve Trie SOC and implementation

--- app/src/main/java/nz/ac/aucklanduni/softeng306/team17/galleria/domain/model/SearchAutocompleteTerms.java
+++ app/src/main/java/nz/ac/aucklanduni/softeng306/team17/galleria/domain/model/SearchAutocompleteTerms.java
@@ -1,7 +1,9 @@
 package nz.ac.aucklanduni.softeng306.team17.galleria.domain.model;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import nz.ac.aucklanduni.softeng306.team17.galleria.util.TrieNode;
 
@@ -22,11 +24,21 @@ public class SearchAutocompleteTerms {
         words.forEach(this.root::insert);
     }
 
+    /**
+     * Add a search term for indexing.
+     */
     public void addSearchTerm(String term) {
         this.root.insert(term);
     }
 
+    /**
+     * Return all matching terms with the provided prefix.
+     */
     public Set<String> getMatchingTerms(String prefix) {
-        return root.getAutocompleteFor(prefix);
+        TrieNode parent = root.traverseTo(prefix.toCharArray());
+
+        return parent.getKeys().stream()
+                .map(it -> prefix + it)
+                .collect(Collectors.toSet());
     }
 }
--- app/src/main/java/nz/ac/aucklanduni/softeng306/team17/galleria/util/TrieNode.java
+++ app/src/main/java/nz/ac/aucklanduni/softeng306/team17/galleria/util/TrieNode.java
@@ -4,18 +4,18 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Based on the implementation found online. <br/>
  *
  * Credit: @see <a href="https://www.techiedelight.com/implement-trie-data-structure-java">tech delight</a>
  */
-// TODO: This class still needs to be finished for allowing search and printing of words.
 public class TrieNode {
 
     private boolean isLeafNode;
 
-    private Map<Character, TrieNode> children;
+    private final Map<Character, TrieNode> children;
 
 
     public TrieNode() {
@@ -35,27 +35,38 @@ public class TrieNode {
         curr.isLeafNode = true;
     }
 
-    public Set<String> getAllSearchTerms() {
-
-        performDfs(this);
-
-        return new HashSet<>();
+    public Set<String> getKeys() {
+        return performDfs(new HashSet<>(), this, "");
     }
 
-    public Set<String> getAutocompleteFor(String current) {
+    /**
+     * Goes down n levels using the chars[n] array to determine which child to traverse to.
+     */
+    public TrieNode traverseTo(char[] chars) {
+        TrieNode parent = this;
+        for (char c : chars) {
+            parent = parent.children.get(c);
 
-        return new HashSet<>();
-    }
-
-    public Map<Character, TrieNode> getChildren() {
-        return children;
-    }
-
-    private static void performDfs(TrieNode entryNode) {
-        for (Map.Entry<Character, TrieNode> child : entryNode.getChildren().entrySet()) {
-            System.out.println(child.getKey());
-            performDfs(child.getValue());
+            if (parent == null) {
+                return new TrieNode();
+            }
         }
+
+        return parent;
     }
 
+    /**
+     * Helper method to return all words formed from a parent entry node.
+     */
+    private static Set<String> performDfs(Set<String> result, TrieNode entryNode, String prev) {
+        for (Map.Entry<Character, TrieNode> child : entryNode.children.entrySet()) {
+            String current = prev + child.getKey();
+            if (child.getValue().isLeafNode) {
+                result.add(current);
+            }
+            performDfs(result, child.getValue(), current);
+        }
+
+        return result;
+    }
 }
