aead13df07e7e98c2d3471a30738bb03c10fa3c4
Raymond Feng <rf.raymondfeng@gmail.com>
2022-08-12 18:59:54 +1200

    Add SearchUseCase

--- app/src/main/java/nz/ac/aucklanduni/softeng306/team17/galleria/domain/repo/ISearchRepository.java
+++ app/src/main/java/nz/ac/aucklanduni/softeng306/team17/galleria/domain/repo/ISearchRepository.java
@@ -1,8 +1,18 @@
 package nz.ac.aucklanduni.softeng306.team17.galleria.domain.repo;
 
+import java.util.Set;
+
 import nz.ac.aucklanduni.softeng306.team17.galleria.domain.model.SearchAutocompleteTerms;
 
 /**
  * {@link IRepository} for {@link SearchAutocompleteTerms}.
  */
-public interface ISearchRepository extends IRepository<SearchAutocompleteTerms> {}
+public interface ISearchRepository extends IRepository<SearchAutocompleteTerms> {
+
+    SearchAutocompleteTerms get();
+
+    Set<String> getPopular(int limit);
+
+    String create(String searchTerm);
+
+}
