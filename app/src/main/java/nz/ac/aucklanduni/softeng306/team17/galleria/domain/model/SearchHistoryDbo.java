352bb12cc82d7b65c284f15fc218172cfd12cad1
Brendan Zhou <brendanzhou.99@gmail.com>
2022-08-17 18:15:10 +1200

    Implemented SearchHistoryDbo

--- app/src/main/java/nz/ac/aucklanduni/softeng306/team17/galleria/domain/model/SearchHistoryDbo.java
+++ app/src/main/java/nz/ac/aucklanduni/softeng306/team17/galleria/domain/model/SearchHistoryDbo.java
@@ -1,4 +1,39 @@
 package nz.ac.aucklanduni.softeng306.team17.galleria.domain.model;
 
-public class SearchHistoryDbo {
+import java.util.list;
+
+public class SearchHistoryDbo() {
+    private String id;
+    private String name;
+    private String searchTerm;
+
+    public SearchHistoryDbo(String id, String name, String searchTerm) {
+        this.id = id;
+        this.name = name;
+        this.searchTerm = searchTerm;
+    }
+
+    public getId() { 
+        return id;
+    }
+
+    public setId(String id) {
+        this.id = id;
+    }
+
+    public getName() {
+        return name;
+    }
+
+    public setName(String name) {
+        this.name = name;
+    }
+
+    public getSearchTerm() {
+        return searchTerm;
+    }
+
+    public setSearchTerm(String searchTerm) { 
+        this.searchTerm = searchTerm;
+    }
 }
