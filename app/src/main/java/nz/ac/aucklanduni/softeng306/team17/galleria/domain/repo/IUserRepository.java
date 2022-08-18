733d0772645a0a3d371b42c802755a68dccee33d
Brendan-Z <brendanzhou.99@gmail.com>
2022-08-18 21:19:52 +1200

    Implemented required functionality for product usecases

--- app/src/main/java/nz/ac/aucklanduni/softeng306/team17/galleria/domain/repo/IUserRepository.java
+++ app/src/main/java/nz/ac/aucklanduni/softeng306/team17/galleria/domain/repo/IUserRepository.java
@@ -1,6 +1,9 @@
 package nz.ac.aucklanduni.softeng306.team17.galleria.domain.repo;
 
+import java.util.List;
+
 import nz.ac.aucklanduni.softeng306.team17.galleria.domain.model.User;
+import nz.ac.aucklanduni.softeng306.team17.galleria.domain.model.product.Product;
 
 /**
  * {@link IRepository} for {@link User} domain model.
@@ -9,6 +12,5 @@ public interface IUserRepository extends IRepository<User> {
 
     User getByEmail(String email);
 
-    public List<Product> getProductsByUser(String userId) {
-    }
+    public List<Product> getProductsByUser(String userId);
 }
--- app/src/main/java/nz/ac/aucklanduni/softeng306/team17/galleria/domain/usecase/ProductUseCase.java
+++ app/src/main/java/nz/ac/aucklanduni/softeng306/team17/galleria/domain/usecase/ProductUseCase.java
@@ -1,44 +1,42 @@
 package nz.ac.aucklanduni.softeng306.team17.galleria.domain.usecase;
-
-
-import java.util.ArrayList;
 import java.util.List;
 
 
-import nz.ac.aucklanduni.softeng306.team17.galleria.domain.repo.AbstractProductRepository;
+import nz.ac.aucklanduni.softeng306.team17.galleria.domain.model.Category;
+import nz.ac.aucklanduni.softeng306.team17.galleria.domain.repo.IProductRepository;
 import nz.ac.aucklanduni.softeng306.team17.galleria.domain.model.product.Product;
 import nz.ac.aucklanduni.softeng306.team17.galleria.domain.repo.IUserRepository;    
 
 public class ProductUseCase {
-    private final AbstractProductRepository productRepo;
+    private final IProductRepository productRepo;
     private final IUserRepository userRepo;
 
-    public ProductUseCase(AbstractProductRepository productRepo, IUserRepository userRepo) {
+    public ProductUseCase(IProductRepository productRepo, IUserRepository userRepo) {
         this.productRepo = productRepo;
         this.userRepo = userRepo;
     }
 
-//    public String getProductById(String id) {
-//        return productRepo.getById(id);
-//    }
+    public Product getProductById(String id) {
+        return productRepo.get(id);
+    }
 
-//    public ArrayList listAllProducts() {
-//        return new ArrayList<Product>(productRepo.getListAllProducts());
-//    }
+    public List<Product> listAllProducts() {
+        return productRepo.listAll();
+    }
 
-//    public List<Product> listProductsByCategory(Category category)) {
-//        return new ArrayList<Product>(productRepo.listByCategory(category));
-//    }
+    public List<Product> listProductsByCategory(Category category) {
+        return productRepo.listByCategory(category);
+    }
 
     public List<Product> listBySearchString(String searchString) {
-        return new ArrayList<Product>(productRepo.listSortByNameMatch(searchString));
+        return productRepo.listSortByNameMatch(searchString);
     }
 
     public List<Product> listTopRatedProduct(int limit) {
-        return new ArrayList<Product>(productRepo.listSortByRating(limit));
+        return productRepo.listSortByRating(limit);
     }
 
     public List<Product> listSavedProductsByUser(String userId) {
-        return new ArrayList<>(userRepo.getProductsByUser(userId));
+        return userRepo.getProductsByUser(userId);
     }
 }
