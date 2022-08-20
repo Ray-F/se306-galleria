7c6cf401e9b0c3bee6116fbca81af07bb4294ee7
Raymond Feng <rf.raymondfeng@gmail.com>
2022-08-21 05:20:33 +1200

    Make MainActivity not use bindings

--- app/src/main/java/nz/ac/aucklanduni/softeng306/team17/galleria/view/MainActivity.java
+++ app/src/main/java/nz/ac/aucklanduni/softeng306/team17/galleria/view/MainActivity.java
@@ -4,28 +4,28 @@ import android.os.Bundle;
 
 import androidx.appcompat.widget.Toolbar;
 
-import nz.ac.aucklanduni.softeng306.team17.galleria.databinding.ActivityMainBinding;
-import nz.ac.aucklanduni.softeng306.team17.galleria.domain.model.Category;
+import com.google.android.material.appbar.AppBarLayout;
+
+import nz.ac.aucklanduni.softeng306.team17.galleria.R;
 
 public class MainActivity extends SearchBarActivity {
 
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Link XML elements with code
+        setContentView(R.layout.activity_main);
+        AppBarLayout layout = findViewById(R.id.topBarLayout);
+
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
 
-        setContentView(binding.getRoot());
-
-        Toolbar toolbar = (Toolbar) binding.topBarLayout.getRoot().getChildAt(0);
+        Toolbar toolbar = (Toolbar) layout.getChildAt(0);
 
         loadToolbar(toolbar);
 
-        Intent categoryIntent = new Intent(this, CategoryResultActivity.class);
+//        Intent categoryIntent = new Intent(this, CategoryResultActivity.class);
 //        categoryIntent.putExtra("productId", "QcVejefcac104q3pOWUu");
-        categoryIntent.putExtra("CATEGORY", Category.PHOTOGRAPHIC);
-        startActivity(categoryIntent);
+//        categoryIntent.putExtra("CATEGORY", Category.PHOTOGRAPHIC);
+//        startActivity(categoryIntent);
     }
 
 }
