d43a2f3d4a85a1ad7f7e19d27a62b603eb3dbe96
Raymond Feng <rf.raymondfeng@gmail.com>
2022-08-21 18:37:46 +1200

    Improve method for populating database items

--- app/src/main/java/nz/ac/aucklanduni/softeng306/team17/galleria/GalleriaApplication.java
+++ app/src/main/java/nz/ac/aucklanduni/softeng306/team17/galleria/GalleriaApplication.java
@@ -1,19 +1,23 @@
 package nz.ac.aucklanduni.softeng306.team17.galleria;
 
 import android.app.Application;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import org.jetbrains.annotations.TestOnly;
-
-import java.util.ArrayList;
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import nz.ac.aucklanduni.softeng306.team17.galleria.data.ProductRepository;
 import nz.ac.aucklanduni.softeng306.team17.galleria.di.DIProvider;
 import nz.ac.aucklanduni.softeng306.team17.galleria.domain.model.Category;
 import nz.ac.aucklanduni.softeng306.team17.galleria.domain.model.CurrencyCode;
-import nz.ac.aucklanduni.softeng306.team17.galleria.domain.model.product.PhotographicArt;
+import nz.ac.aucklanduni.softeng306.team17.galleria.domain.model.product.AlbumArt;
 import nz.ac.aucklanduni.softeng306.team17.galleria.domain.model.product.Product;
 
 public class GalleriaApplication extends Application {
@@ -29,20 +33,49 @@ public class GalleriaApplication extends Application {
         diProvider = new DIProvider();
 
 
-//        populateDummyData();
+        // TODO: Uncomment this line and run application to populate data
+        // populateDummyData();
     }
 
-    public void populateDummyData() {
-        String base64 = "";
-        byte[] heroImg = java.util.Base64.getDecoder().decode(base64);
-        Product product = new PhotographicArt("", "White",
-                                              "Here lay the misty mountains covered in snow", "Long description", heroImg,
-                                              new ArrayList<>(), 100, CurrencyCode.NZD, "#dddddd",
-                                              4.7f, 200, 1, 3,
-                                              Category.PHOTOGRAPHIC, "Canon R5, 24-70mm F2.8");
+    private void populateDummyData() {
+        // TODO: Usage Add image into r/drawable and reference their ID's where needed
+        //  (after long description and inside Stream.of() for other images.
+        //  Put the product inside the addProducts method below
+        Product product1 = new AlbumArt("", "Test Example",
+                                        "Test tagline", "Long description", toByteArr(R.drawable.galleria_logo),
+                                        Stream.of(R.drawable.aigenerated, R.drawable.album).map(this::toByteArr).collect(Collectors.toList()),
+                                        100, CurrencyCode.NZD, "#dddddd",
+                                        4.7f, 200, 1, 3,
+                                        Category.ALBUM, "Raymond Feng");
 
+        Product product2 = new AlbumArt("", "Test Example no. 2",
+                                        "Test tagline", "Long description", toByteArr(R.drawable.galleria_logo),
+                                        Stream.of(R.drawable.aigenerated, R.drawable.album).map(this::toByteArr).collect(Collectors.toList()),
+                                        100, CurrencyCode.NZD, "#dddddd",
+                                        4.7f, 200, 1, 3,
+                                        Category.ALBUM, "Raymond Feng");
+
+        // TODO: Remember to add product into here
+        addProducts(product1);
+    }
+
+    private byte[] toByteArr(int rId) {
+        Bitmap image = BitmapFactory.decodeResource(getResources(), rId);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 60, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+
+        String base64 = Base64.getEncoder().encodeToString(byteArray).replaceFirst("data:image/jpeg;base64", "");
+        return java.util.Base64.getDecoder().decode(base64);
+    }
+
+
+    private void addProducts(Product ... products) {
         ProductRepository productRepo = new ProductRepository(FirebaseFirestore.getInstance());
-        productRepo.create(product);
+
+        Arrays.stream(products).forEach(productRepo::create);
     }
+
+
 
 }
